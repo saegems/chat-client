983027423f698ca65f80af4d6332b3dcb1eed57d
Azeem Pinjari <113772951+azeem30@users.noreply.github.com>
2025-09-10 11:19:49 +0530

    Update window size

--- main.py
+++ main.py
@@ -45,8 +45,8 @@ class MainWindow(QMainWindow):
         self.setWindowFlags(Qt.FramelessWindowHint)
         self.setAttribute(Qt.WA_TranslucentBackground)
 
-        self.setMinimumSize(600, 600)
-        self.resize(600, 600)
+        self.setMinimumSize(800, 1200)
+        self.resize(800, 1200)
 
         self.central_widget = QWidget()
         self.central_widget.setObjectName("CentralWidget")
